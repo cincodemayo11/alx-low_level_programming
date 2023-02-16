f06bfd380d387ad4cb3dcb52a4c79bc30bfbeff6
cincodemayo11 <kwi6441@gmail.com>
2023-02-16 07:47:57 -0800

    using puts in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,8 @@
 
 int main(void)
 {
+	/*This program prints ""Programming is like building a multilingual puzzle" using the ~puts~ the function*/
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
