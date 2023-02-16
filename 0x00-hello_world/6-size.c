b96c26f4f48c16773a19dda33b1f6b140da5728a
cincodemayo11 <kwi6441@gmail.com>
2023-02-16 11:06:16 -0800

    size of different variable types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main- Entry point
+  *
+  *Description: prints the size of various types on this computer
+  *
+  *Return: ALways 0 (Success)
+  */
+
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
+	printf("Size of an int: %ld byte(s)\n", sizeof(intType);
+	printf("Size of a long int: %ld byte(s)\n", sizeof(longintType);
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongintType);
+	printf("Size of a float: %ls byte(s)\n", sizeof(floatType);
+}
+
