1cfd2cd4724dae44ca95535be06cbb514b8cc180
cincodemayo11 <kwi6441@gmail.com>
2023-02-16 11:10:46 -0800

    size of different variable types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,12 @@
 
 int main(void)
 {
+	char charType;
+	int intType
+	long longintType;
+	long long longlongintType;
+	float floatType;
+	
 	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
 	printf("Size of an int: %ld byte(s)\n", sizeof(intType);
 	printf("Size of a long int: %ld byte(s)\n", sizeof(longintType);
