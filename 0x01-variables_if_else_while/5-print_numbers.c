25566190c0eb90230cb306dfa57a8b391ae1e1a8
cincodemayo11 <kwi6441@gmail.com>
2023-02-17 05:36:51 -0800

    print 0 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *prints numbers to 10
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+}
