909f0d826eef08e048855042ff4f2eec8193aade
cincodemayo11 <kwi6441@gmail.com>
2023-02-17 02:53:39 -0800

    comparing numbers using if else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,17 +18,17 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("%d is positive", n);
+		printf("%d is positive\n", n);
 		return (0);
 	}
 	if (n < 0)
 	{
-		printf("%d is negative", n);
+		printf("%d is negative\n", n);
 		return (0);
 	}
 	else
 	{
-		printf("%d is zero", n);
+		printf("%d is zero\n", n);
 	}
 	/* your code goes there */
 	return (0);
