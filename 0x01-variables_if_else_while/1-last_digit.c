504e4ecc9e5c52610299ca0ec834c4ce0c5150c8
cincodemayo11 <kwi6441@gmail.com>
2023-02-17 04:10:56 -0800

    comparing ints with if else

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,17 +15,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("Last digit of %d is %d and ", n, (n % 10));
-	if (n % 10 > 5)
+	if (n % 10 == 0)
+	{
+		printf("and is 0");
+	}
+	else if (n % 10 > 5)
 	{
 		printf("is greater than 5\n");
 	}
 	else if (n % 10 < 6 && n != 0)
 	{
 		printf("is less than 6 and not 0\n");
-	}
-	else if (n % 10 == 0)
-	{
-		printf("is 0\n");
 	}
 	return (0);
 }
