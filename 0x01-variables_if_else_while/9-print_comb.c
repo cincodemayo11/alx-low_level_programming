07c5a3976cb747c937c8c00b3164b3b588e4c7be
cincodemayo11 <kwi6441@gmail.com>
2023-02-17 06:45:10 -0800

    putchar single numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,10 +11,16 @@ int main(void)
 
 	for (; i <= 57; i++)
 	{
-		putchar(i);
-		putchar(',');
-		putchar(' ');
+		if (i != 57)
+		{
+			putchar(i);
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			putchar(i);
+		}
 	}
-	putchar('\n');
 	return (0);
 }
