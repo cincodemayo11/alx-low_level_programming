c4773310f6df3225ac48eed87c02e03ab535782d
cincodemayo11 <kwi6441@gmail.com>
2023-02-21 03:32:53 -0800

    fixed betty description

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ *print_alphabet_x10 - Entry point
+ *
+ *Putchars the alphabet ten times followed by a newline
+ *
+ *Return: Always 0 (Success.)
+ */
+
 void print_alphabet_x10(void)
 {
 	int i = 0;
