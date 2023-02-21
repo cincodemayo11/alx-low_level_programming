b5f94c4b7b1d8a98d1de25bd7885a314b992d205
cincodemayo11 <kwi6441@gmail.com>
2023-02-21 03:30:38 -0800

    fixed betty style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,16 +2,16 @@
 
 void print_alphabet_x10(void)
 {
-        int i = 0;
-        char c;
+	int i = 0;
+	char c;
 
-        while ( i < 10)
-        {
-               for (c = 'a'; c <= 'z'; c++)
-                {
-                         _putchar(c);
-                }
-                i++;
-                _putchar('\n');
-        }
+	while (i < 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		i++;
+		_putchar('\n');
+	}
 }
