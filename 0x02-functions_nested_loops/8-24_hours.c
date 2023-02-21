c3ca8b0485908553604824b567ae191ca87e67b7
cincodemayo11 <kwi6441@gmail.com>
2023-02-21 07:06:45 -0800

    got the code working

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+void jack_bauer(void)
+{
+
+	int j = 0;
+	int i = 0;
+
+	while (i < 24)
+	{
+		j = 0;
+
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+
+			j++;
+		}
+		i++;
+	}
+}
