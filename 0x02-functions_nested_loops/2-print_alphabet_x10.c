961fe75ff0e86e3cbbc1aa7d3f79756a4270dc7a
cincodemayo11 <kwi6441@gmail.com>
2023-02-21 03:26:33 -0800

    putchars the alphabet ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+        int i = 0;
+        char c;
+
+        while ( i < 10)
+        {
+               for (c = 'a'; c <= 'z'; c++)
+                {
+                         _putchar(c);
+                }
+                i++;
+                _putchar('\n');
+        }
+}
