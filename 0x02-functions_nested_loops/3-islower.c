9e589386e8527371d6390801b210cd387452a73d
cincodemayo11 <kwi6441@gmail.com>
2023-02-21 04:03:54 -0800

    fixed betty description

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ *_islower - Checks if letter is lower case
+ *
+ *@c: The character in ASCII code
+ *
+ *Return : 1 if lower. 0 if not
+ */
+
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
