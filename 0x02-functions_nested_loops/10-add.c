6902d38dbd030f148c68670728bcd0da3f2b7e59
cincodemayo11 <kwi6441@gmail.com>
2023-02-21 12:39:05 -0800

    fixing betty

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+/**
+ * add - Adds two integers and returns
+ * the result.
+ *
+ * @i: number one.
+ * @j: number two.
+ *
+ * Return: Add of number one and number two.
+ */
+
 int add(int i, int j)
 {
 	int result;
