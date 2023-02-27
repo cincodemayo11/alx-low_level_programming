25df082e25b0aaf2da4206450b35aa1858cc6057
cincodemayo11 <kwi6441@gmail.com>
2023-02-27 12:48:02 -0800

    fixing betty description

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ *_strcpy - copies a string
+ *
+ *@src: where we're copying from
+ *@dest: where we're copying to
+ *
+ *Return: the pointer to dest
+ */
+
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
