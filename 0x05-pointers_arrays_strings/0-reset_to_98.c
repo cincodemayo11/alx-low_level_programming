de2e05b0e75bf520de67f880e6d695c47131c816
cincodemayo11 <kwi6441@gmail.com>
2023-02-27 04:21:24 -0800

    1st attempt

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - changes int to 98
+ *
+ *@n: the pointer to the int
+ *
+ *Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+ALX tasks on pointers, arrays and strings.
