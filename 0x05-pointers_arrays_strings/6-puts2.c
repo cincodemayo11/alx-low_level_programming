63d1518b0aa3f285f74d529bbc0665bb77c70727
cincodemayo11 <kwi6441@gmail.com>
2023-02-27 11:00:53 -0800

    1st attempt

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *puts2 - prints every other char of a string
+ *
+ *@str: the string in question
+ *
+ *Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (; *(str + i) != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(*(str + i));
+		}
+		else
+		{
+			continue;
+		}
+	}
+	_putchar('\n');
+}
