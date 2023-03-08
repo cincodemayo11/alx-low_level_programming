36988996769df0c00d25223126983e38b99b41e5
cincodemayo11 <kwi6441@gmail.com>
2023-03-08 03:39:40 -0800

    fixed betty errors

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *factorial - calculates the factorial of a non-neg number
+ *
+ *@n: the non-neg number
+ *
+ *Return: the factorial
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
