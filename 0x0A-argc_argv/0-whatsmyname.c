183c86e966ccc783e1dde8044be1e4ac91cf6cde
cincodemayo11 <kwi6441@gmail.com>
2023-03-10 13:04:06 -0800

    fixing betty descriptions

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ *main - prints out the first commandline argument
+ *
+ *@argc: thw number of arguments
+ *@argv: the actual arguments
+ *
+ *Return: 0 (Always success.)
+ */
+
 int main(int argc __attribute__((unused)), char *argv[])
 {
 
