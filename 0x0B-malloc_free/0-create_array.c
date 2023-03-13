19581b22bf9c6f701b53f4faeaa1f34d1bef0935
cincodemayo11 <kwi6441@gmail.com>
2023-03-13 05:49:59 -0700

    Automatic commit message

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,21 +1,30 @@
 #include <stdlib.h>
+/**
+ *create_array - craetes a char array and initializes it with char c
+ *
+ *@size: the size of the array
+ *@c: the char we're initializing with
+ *
+ *Return: A pointer to the array.
+ */
 
 char *create_array(unsigned int size, char c)
 {
 	char *i;
-	int j;
+	unsigned int j;
 
-	i = (char*) malloc(sizeof(char) * size);
+	i = (char *) malloc(sizeof(char) * size);
 
 	if (size == 0)
 	{
-		return('\0');
+		return ('\0');
 	}
 	else
-		for (j = 0; i[j] != '\0'; j++)
+		for (j = 0; j < size; j++)
 		{
 			i[j] = c;
 		}
 
 	return (i);
+	free(i);
 }
