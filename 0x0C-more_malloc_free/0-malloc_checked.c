827dc220fd18565d24473105c4a2113c9fd0a395
cincodemayo11 <kwi6441@gmail.com>
2023-03-16 04:46:15 -0700

    Automatic commit message

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *malloc_checked - allocates memory using malloc and checks validity
+ *
+ *@b: the size of the memory
+ *
+ *Return: pointer to the memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b * sizeof(int));
+
+	if (ptr == NULL)
+	{
+		free(ptr);
+		exit(98);
+	}
+	return (ptr);
+}
