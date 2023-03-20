c94325ddef4cda101b7ab29adb2026e84f1591d2
cincodemayo11 <kwi6441@gmail.com>
2023-03-20 07:50:17 -0700

    Automatic commit message

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ *print_dog - prints the dog'd details
+ *
+ *@d: struct containing dog's details
+ *
+ *Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (!(d->name))
+		{
+			printf("Name: (nil)\n");
+		}
+		else
+		{
+			printf("Name: %s\n", d->name);
+		}
+		if (!(d->age))
+		{
+			printf("Age: (nil)\n");
+		}
+		else
+		{
+			printf("Age: %.1f\n", d->age);
+		}
+		if (!d->owner)
+		{
+			printf("Age: (nil)\n");
+		}
+		else
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+	}
+}
