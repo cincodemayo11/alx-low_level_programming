dce389e42a091a42afacd565a72f0e2df2cc3116
cincodemayo11 <kwi6441@gmail.com>
2023-03-20 08:10:39 -0700

    Automatic commit message

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,9 +11,9 @@
 
 void print_dog(struct dog *d)
 {
-	if (d)
+	if (d != NULL)
 	{
-		if (!(d->name))
+		if ((d->name) == NULL)
 		{
 			printf("Name: (nil)\n");
 		}
@@ -21,15 +21,10 @@ void print_dog(struct dog *d)
 		{
 			printf("Name: %s\n", d->name);
 		}
-		if (!(d->age))
-		{
-			printf("Age: (nil)\n");
-		}
-		else
-		{
-			printf("Age: %f\n", d->age);
-		}
-		if (!(d->owner))
+
+		printf("Age: %f\n", d->age);
+
+		if ((d->owner) == NULL)
 		{
 			printf("Age: (nil)\n");
 		}
