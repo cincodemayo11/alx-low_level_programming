85c437509ef315ab4a2a1381dd9563fdf75e669d
cincodemayo11 <kwi6441@gmail.com>
2023-03-20 11:28:12 -0700

    Automatic commit message

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,7 +18,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int namelen;
 	int i;
 
-	new = malloc(sizeof(new));
+	new = malloc(sizeof(*new));
 	if (new == NULL || name == NULL || owner == NULL)
 	{
 		free(new);
