281653ab41580645df576f04d19dc83261a97109
cincodemayo11 <kwi6441@gmail.com>
2023-03-20 06:45:38 -0700

    Automatic commit message

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef _DOG_
 #define _DOG_
 
+/**
+ *struct dog - Details about a good boy :)
+ *
+ *@name: name of the good boy
+ *@age: his age
+ *@owner: his owner
+ *
+ */
+
 struct dog
 {
 	char *name;
