dbe99d1c6aba8c1ae5db324f5ea189e7afd078ec
cincodemayo11 <kwi6441@gmail.com>
2023-03-22 06:40:03 -0700

    Automatic commit message

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ *print_name - call-backs a function that prints a name
+ *
+ *@name: The name
+ *@f: function that actually prints
+ *
+ *Returns: nada
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
