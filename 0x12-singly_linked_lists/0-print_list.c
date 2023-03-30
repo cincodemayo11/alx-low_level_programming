23dc428e1a8dad625b631a9e55b08a30f375a7e6
cincodemayo11 <kwi6441@gmail.com>
2023-03-30 14:57:01 -0700

    Automatic commit message

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ *print_list - prints all the elements of a list
+ *
+ *@h: the list
+ *
+ *Return: number of elements
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t num;
+
+	num = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		num++;
+	}
+	return (num);
+}
