76d0c8a653979b5bd660ff0aad74cdd80c744b59
cincodemayo11 <kwi6441@gmail.com>
2023-04-10 15:22:39 -0700

    Automatic commit message

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,6 +27,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
+	if (!text_content)
+	{
+		text_content = "";
+	}
+
 	written = write(openf, text_content, strlen);
 
 	if (written != strlen)
