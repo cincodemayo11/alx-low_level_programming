943cdbb9f36a908fdac681d40f7b6cfe5d0afdc6
cincodemayo11 <kwi6441@gmail.com>
2023-04-10 05:13:06 -0700

    Automatic commit message

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,21 +4,16 @@
  *get_bit - return value of a bit at index
  *
  *@n: the int being worked on
- *index: the index of the bit
+ *@index: the index of the bit
  *
  *Return: the value of the bit at index index or -1 if an error occured
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (!n && index < 64)
-	{
-		return (0);
-	}
 	if (n)
 	{
 		return ((n >> index) & 1);
 	}
-	else
-		return (-1);
+	return (-1);
 }
