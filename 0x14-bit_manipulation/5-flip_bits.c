b2bae5108e787e5d8c1c22b366ed8784cbd356a4
cincodemayo11 <kwi6441@gmail.com>
2023-04-10 10:31:54 -0700

    Automatic commit message

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *flip_bits - number of bits you would need to flip a num to another
+ *
+ *@n: one number
+ *@m: the other number
+ *
+ *Return: number of bits you would need to flip to get from one num to another
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int i = n ^ m;
+	unsigned int j = 0;
+
+	while (i != 0)
+	{
+		j += i & 1;
+		i >>= 1;
+	}
+	return (j);
+}
